f3b100c824aa34e1c4911c7af01618805ef0b1a7
Audrey <aerv@ic.ufal.br>
2020-11-03 15:59:59 -0300

    resolução de equação

---
+++ fundamentos/DesafioAritmeticos.java
@@ -0,0 +1,21 @@
+package fundamentos;
+
+public class DesafioAritmeticos {
+	public static void main(String[] args) {
+		double numeradorA = Math.pow(6 * (3 + 2), 2);
+		double denominadorA = 3 * 2;
+		
+		double numeradorB = (1 - 5) * (2 - 7);
+		double denominadorB = 2;
+		
+		double superiorA = numeradorA / denominadorA;
+		double superiorB = Math.pow(numeradorB / denominadorB, 2);
+		
+		double superior = Math.pow(superiorA - superiorB, 3);
+		double inferior = Math.pow(10, 3);
+		
+		double resultado = superior / inferior;
+		
+		System.out.println("o resultado é " + resultado);
+	}
+}
