7236e00b9661c24511dfbad6eeb9c3a69a9756fe
Audrey <aerv@ic.ufal.br>
2021-07-04 08:00:25 -0300

    Implementando métodos

--- classe/Data.java
+++ classe/Data.java
@@ -4,4 +4,12 @@ public class Data {
 	int dia;
 	int mes;
 	int ano;
+	
+	String obterDataFormatada() {
+		return String.format("%d/%d/%d\n", dia, mes, ano);
+	} // esse tipo de método funcionará em diversas aplicações
+	
+	void imprimirDataFormatada() {
+		System.out.println(obterDataFormatada());
+	} // esse tipo de método só funcioará dentro de uma aplicação em terminal
 }
