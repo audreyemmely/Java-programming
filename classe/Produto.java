5a2a55dd684ff61738f189280216b5633c2229b3
Audrey <aerv@ic.ufal.br>
2021-07-04 07:51:12 -0300

    Implementando métodos

--- classe/Produto.java
+++ classe/Produto.java
@@ -4,4 +4,13 @@ public class Produto {
 	String nome;
 	double preco;
 	double desconto;
+	
+	double precoComDesconto() {
+		return preco * (1 - desconto);
+	}
+	
+	double precoComDesconto(double descontoDoGerente) {
+		return preco * (1 - desconto + descontoDoGerente);
+	}
+	// é possível existir dois métodos com mesmo nome, contanto que eles tenham qtdd de variáveis diferentes
 }
